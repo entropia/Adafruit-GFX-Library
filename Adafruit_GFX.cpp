0797d24650332d7a72d7555c41f1025eb2d3f77f
Anton Schirg <anton.schirg@posteo.de>
2017-05-24 16:57:28 +0200

    Remove pgm_read_{byte,word,dword} defines
    for compatibility with esp8266 sdk 2.1.0

--- Adafruit_GFX.cpp
+++ Adafruit_GFX.cpp
@@ -39,20 +39,6 @@ POSSIBILITY OF SUCH DAMAGE.
   #include <pgmspace.h>
 #endif
 
-// Many (but maybe not all) non-AVR board installs define macros
-// for compatibility with existing PROGMEM-reading AVR code.
-// Do our own checks and defines here for good measure...
-
-#ifndef pgm_read_byte
- #define pgm_read_byte(addr) (*(const unsigned char *)(addr))
-#endif
-#ifndef pgm_read_word
- #define pgm_read_word(addr) (*(const unsigned short *)(addr))
-#endif
-#ifndef pgm_read_dword
- #define pgm_read_dword(addr) (*(const unsigned long *)(addr))
-#endif
-
 // Pointers are a peculiar case...typically 16-bit on AVR boards,
 // 32 bits elsewhere.  Try to accommodate both...
 
@@ -69,6 +55,7 @@ POSSIBILITY OF SUCH DAMAGE.
 #ifndef _swap_int16_t
 #define _swap_int16_t(a, b) { int16_t t = a; a = b; b = t; }
 #endif
+
 
 Adafruit_GFX::Adafruit_GFX(int16_t w, int16_t h):
 WIDTH(w), HEIGHT(h)
